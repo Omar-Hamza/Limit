9640dc086eb3fc6323edee9052967daa0d22f73d
Omar-Hamza <omar.mohamed.hmza@gmail.com>
2024-03-26 20:37:16 +0200

    Testing different destination urls

--- Subsystems/Interface/grpcpage/SendActiontoPy.js
+++ Subsystems/Interface/grpcpage/SendActiontoPy.js
@@ -7,7 +7,7 @@ function callPythonFunction(param) {
         body: JSON.stringify({ param }),
     };
 
-    return fetch('../../grpcController/', requestOptions)
+    return fetch('/Subsystems/grpcController/call_python_function', requestOptions)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
